97d3805de785bc1e0d4558ae954b7497cd585a2d
jeffzhkw <30642720+jeffzhkw@users.noreply.github.com>
2018-01-02 09:22:25 +0800

    CodingBat classwork

--- CodingBat/AP1_Jeff.java
+++ CodingBat/AP1_Jeff.java
@@ -12,3 +12,20 @@ public boolean scoresIncreasing(int[] scores) {
   return true; 
   
 }
+
+//Q2
+public boolean scores100(int[] scores) {
+  for (int i = 0;  i < scores.length-1; i++){
+     
+    if (scores[i] == scores[i + 1] && scores[i + 1]==100){
+    
+    return true;
+   
+    }
+    
+  }
+  
+  return false;
+}
+
+//Q3
